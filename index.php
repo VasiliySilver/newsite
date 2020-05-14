eb6840aac402e3436b26324d7103c60a8887c1ef
VasiliySilver <s555133@mail.ru>
2020-05-14 15:11:49 +0300

    Add project

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Новости обо всём</title>
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+<body>
+    <header>
+        <div id="logo">
+            <a href="/" title="Перейти на главную">
+                <span>Н</span>овости
+            </a>
+        </div>
+        <div id="menuHead">
+            <a href="/about.php">
+                <div>О нас </div>
+            </a>
+            <a href="/about.php">
+                <div>Обратная связь</div>
+            </a>
+        </div>
+        <div id="regAuth">
+            <a href="/reg.php">Регистрация</a> | <a href="/auth.php">Авторизация</a>
+        </div>
+
+    </header>
+
+</body>
+</html>
